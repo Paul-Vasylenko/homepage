bac7c43ed41e44ca732a9c98be68a25c07ef59de
Paul Vass <maincrafter1307@gmail.com>
2021-05-01 16:48:31 +0300

    animation added

---
+++ js/index.js
@@ -0,0 +1,6 @@
+document.addEventListener('DOMContentLoaded', function(){
+    setTimeout(()=>{
+        const jacksonImg = document.querySelector(".header__img");
+        jacksonImg.style.opacity = 1;
+    }, 200)
+})
